d86b4d276939d187c57299d198fe98b99628bf09
shashipratapsingh <itpro8577@gmail.com>
2025-04-03 17:06:12 +0530

    git registory code

---
+++ src/test/java/ApiGatewayService/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
