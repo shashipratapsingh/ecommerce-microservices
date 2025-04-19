7685223e692f422fb8dc6406408252a1bdf1014f
shashipratapsingh <itpro8577@gmail.com>
2025-04-19 13:37:39 +0530

    added docker

---
+++ src/main/java/ProfileService/HealthCheck/CustomHealthIndicator.java
@@ -0,0 +1,15 @@
+package ProfileService.HealthCheck;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        // Custom health logic
+        return Health.up().withDetail("custom", "Custom Health Indicator , Profile Service").build();
+    }
+}
